7a0b43623991a2c3a4702a586e931fd17a1d9ccf
danil <danil.danilka500@gmail.com>
2026-09-22 17:12:32 +0300

    всё остальное

---
+++ src/test/java/ru/kpfu/app/KpfuApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kpfu.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KpfuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
